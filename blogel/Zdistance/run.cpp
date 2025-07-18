80a01543785087b1fd85b0da1648064f74f329cd
1fyxytx <106674245+1fyxytx@users.noreply.github.com>
2025-07-18 16:49:29 +0800

    Update run.cpp

--- blogel/Zdistance/run.cpp
+++ blogel/Zdistance/run.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 	init_workers();
-	SetLCR_Construction(  "/mnt/data/zyy/Full/indochina.graph",
-				          "/mnt/data/zyy/part/indochina/p10",
-			              "/mnt/data/zyy/LCHIndex/it2004/index_",
+	SetLCR_Construction(  "/blogel/data/indochina.graph",
+				          "/blogel/data/indochina/p10",
+			              "/blogel/index/indochina/index_",
 			        1049, 123980, 32); //  Distri4hop 索引构建 的输入
 
 	worker_finalize();
